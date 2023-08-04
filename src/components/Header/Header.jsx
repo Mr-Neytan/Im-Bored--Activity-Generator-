1728b04b91fd381a3177d0d5d865a8faf225fc19
nathaniel-mercado <nmercado0517@gmail.com>
2023-08-04 12:30:29 +0800

    Added Header and Adjusted Font Sizes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div class="header">
+      <h1><b>Activity Generator</b></h1>
+      </div>
+  )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Activity from './components/Activity/activity.jsx'
+import Header from './components/Header/Header.jsx'
 import './index.css'
+
+let header_root = ReactDOM.createRoot(document.getElementById("head"))
+header_root.render(<Header/>)
 
 let root = ReactDOM.createRoot(document.getElementById("root"))
 let content = <Activity/>
